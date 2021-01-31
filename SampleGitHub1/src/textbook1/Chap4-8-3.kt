69faa0ede3dbc2d8e345c6e830b93c92589dc6fe
TestUser <toshix.ishix.developer@outlook.jp>
2021-01-31 16:08:28 +0900

    Commit

--- SampleGitHub1/src/textbook1/Chap4-8-3.kt
+++ SampleGitHub1/src/textbook1/Chap4-8-3.kt
@@ -1,10 +1,12 @@
 package textbook1
 
+// オブジェクト式とSAM変換
 fun main(args: Array<String>) {
-    example_anonymous()
-}
 
-// オブジェクト式
-fun example_anonymous() {
+    // 匿名クラスでの実装例
+
+    // オブジェクト式での実装例
+
+    // ラムダ式での実装例(SAM変換)
 
 }
--- SampleGitHub1/src/textbook1/Chap4-8-8.kt
+++ SampleGitHub1/src/textbook1/Chap4-8-8.kt
@@ -7,6 +7,10 @@ fun main(args: Array<String>) {
 // 拡張関数
 fun example_extendFunction() {
     // Stringクラスに関数を追加してみる
+    // sample1
     fun String.surround() = "[" + this + "]"
     println("Kotlin".surround())
+    // sample2
+    fun String.test(arg: String): Boolean{ return this.equals(arg) }
+    println("Kotlin".test("Kotlin"))
 }
